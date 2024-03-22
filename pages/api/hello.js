b26823d19179fdfbaa641d123a2a264378a1d6fd
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-03-22 15:23:06 +0530

    finished

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import forumData from "../data/fourmData";
+import forumData from "../../data/fourmData";
 
 export default function handler(req, res) {
   res.status(200).json(forumData);
