af8103fe3722e8c955a73d7e315f8a34fd49cf6f
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-03-22 16:59:42 +0530

    finished

--- components/view/Mobile.jsx
+++ components/view/Mobile.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Forum from "../Forum";
 import Stories from "../Stories";
+import axios from "axios";
 
 const Mobile = () => {
   const [forum, setForum] = useState(true);
+  const [fdata, setFdata] = useState([]);
+  useEffect(() => {
+    getFdata();
+  }, []);
+
+  const getFdata = async () => {
+    try {
+      const data = await axios.get(`/api/hello`);
+      setFdata(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="">
       <div className="flex justify-between">
@@ -27,11 +41,23 @@ const Mobile = () => {
       <div>
         {forum && (
           <div className="p-2">
-            <Forum />
+          {fdata.map((d, i) => (
+          <Forum
+            key={i}
+            name={d.user}
+            comment={d.comment}
+            noOfLikes={d.noOfLikes}
+            noOfView={d.noOfView}
+            noOfComments={d.noOfComments}
+          />
+        ))}
           </div>
         )}
         {!forum && (
           <div className="p-2">
+            <Stories />
+            <Stories />
+            <Stories />
             <Stories />
           </div>
         )}
